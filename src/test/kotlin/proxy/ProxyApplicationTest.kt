4fbd737dc50ba14ac391516e765dd5b944daa93b
Ricardo Baumann <gemini.richard@gmail.com>
2017-11-17 10:23:47 +0100

    code cleanup

--- src/test/kotlin/proxy/ProxyApplicationTest.kt
+++ src/test/kotlin/proxy/ProxyApplicationTest.kt
@@ -6,17 +6,17 @@ import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.nhaarman.mockito_kotlin.mock
 import org.assertj.core.api.Assertions
-import org.junit.After
+import org.assertj.core.api.Assertions.*
 import org.junit.Before
 
 import org.junit.Test
 
 class ProxyApplicationTest {
-    val objectMapper = ObjectMapper()
+    private val objectMapper = ObjectMapper()
 
-    val testEntity = objectMapper.createObjectNode().put("title","test").put("body","test")
+    private val testEntity = objectMapper.createObjectNode().put("title","test").put("body","test")!!
 
-    val lookupService : LookupService = mock<LookupService> {
+    private val lookupService : LookupService = mock<LookupService> {
         onGeneric { get("1") }.thenReturn(testEntity)
     }
 
@@ -32,25 +32,16 @@ class ProxyApplicationTest {
         Thread.sleep(1000)
     }
 
-    //
-
     @Test
     fun shouldReturnExistentObject() {
-        val (request, response, result) = "http://localhost:7005/cache/1".httpGet().authenticate("test","test").response()
+        val (_, _, result) = "http://localhost:7005/cache/1".httpGet().authenticate("test","test").response()
         when(result) {
             is Result.Success -> {
-                Assertions.assertThat(objectMapper.readTree(result.get())).isEqualTo(testEntity)
-                println("success")
+                assertThat(objectMapper.readTree(result.get())).isEqualTo(testEntity)
             }
             is Result.Failure -> {
-                Assertions.fail(result.error.message)
+                fail(result.error.message)
             }
         }
     }
-
-    @After
-    fun after() {
-        //proxyApplication.app.stop()
-    }
-
 }
